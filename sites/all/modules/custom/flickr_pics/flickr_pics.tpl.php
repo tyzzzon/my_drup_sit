bc4b77fc8c5a431978581f88460cd456531e7eec
tyzzzon <tyzzon@yandex.ru>
2015-11-30 17:45:02 +0600

    got photoes by date and tags, the beginning of templates

---
+++ sites/all/modules/custom/flickr_pics/flickr_pics.tpl.php
@@ -0,0 +1,65 @@
+
+<div id="gallery" class="ad-gallery">
+  <div class="ad-image-wrapper"><div class="ad-image" style="width: 600px; height: 400px; top: 0px; left: 0px;"><img src="https://www.flickr.com/photos/137868270@N03/22787926573/" width="600" height="400"><p class="ad-image-description" style="width: 586px; bottom: 0px;"><strong class="ad-description-title">A title for 5.jpg</strong><span>This is a nice, and incredibly descriptive, description of the image 5.jpg</span></p></div><img class="ad-loader" src="loader.gif" style="display: block;"><div class="ad-next" style="height: 400px;"><div class="ad-next-image" style="opacity: 0.7; display: none;"></div></div><div class="ad-prev" style="height: 400px;"><div class="ad-prev-image" style="opacity: 0.7; display: none;"></div></div></div>
+  <div class="ad-controls">
+    <p class="ad-info">9 / 13</p><div class="ad-slideshow-controls"><span class="ad-slideshow-start">Start</span><span class="ad-slideshow-stop">Stop</span><span class="ad-slideshow-countdown" style="display: none;"></span></div></div>
+  <div class="ad-nav"><div class="ad-back" style="opacity: 0.6;"></div>
+    <div class="ad-thumbs">
+      <ul class="ad-thumb-list" style="width: 1353px;">
+        <li>
+          <a href="images/1.jpg" class="">
+            <img src="images/thumbs/t1.jpg" class="image0" style="opacity: 0.7;">
+          </a>
+        </li>
+        <li>
+
+        </li>
+        <li>
+          <a href="images/11.jpg">
+            <img src="images/thumbs/t11.jpg" title="A title for 11.jpg" longdesc="http://coffeescripter.com" alt="This is a nice, and incredibly descriptive, description of the image 11.jpg" class="image2" style="opacity: 0.7;">
+          </a>
+        </li>
+        <li>
+          <a href="images/12.jpg" class="">
+            <img src="images/thumbs/t12.jpg" title="A title for 12.jpg" alt="This is a nice, and incredibly descriptive, description of the image 12.jpg" class="image3" style="opacity: 0.7;">
+          </a>
+        </li>
+        <li>
+          <a href="images/13.jpg">
+            <img src="images/thumbs/t13.jpg" title="A title for 13.jpg" alt="This is a nice, and incredibly descriptive, description of the image 13.jpg" class="image4" style="opacity: 0.7;">
+          </a>
+        </li>
+        <li>
+          <a href="images/14.jpg" class="">
+            <img src="images/thumbs/t14.jpg" title="A title for 14.jpg" alt="This is a nice, and incredibly descriptive, description of the image 14.jpg" class="image5" style="opacity: 0.7;">
+          </a>
+        </li>
+        <li>
+          <a href="images/2.jpg" id="t2" class="">
+            <img src="images/thumbs/t2.jpg" title="A title for 2.jpg" alt="This is a nice, and incredibly descriptive, description of the image 2.jpg" class="image6" style="opacity: 0.7;">
+          </a>
+        </li>
+        <li>
+          <a href="images/3.jpg" class="">
+            <img src="images/thumbs/t3.jpg" title="A title for 3.jpg" alt="This is a nice, and incredibly descriptive, description of the image 3.jpg" class="image7" style="opacity: 0.7;">
+          </a>
+        </li>
+        <li>
+          <a href="images/4.jpg">
+            <img src="images/thumbs/t4.jpg" title="A title for 4.jpg" alt="This is a nice, and incredibly descriptive, description of the image 4.jpg" class="image8" style="opacity: 0.7;">
+          </a>
+        </li>
+        <li>
+          <a href="images/5.jpg" class="ad-active">
+            <img src="images/thumbs/t5.jpg" title="A title for 5.jpg" alt="This is a nice, and incredibly descriptive, description of the image 5.jpg" class="image9" style="opacity: 1;">
+          </a>
+        </li>
+        <li>
+          <a href="images/6.jpg" class="">
+            <img src="images/thumbs/t6.jpg" title="A title for 6.jpg" alt="This is a nice, and incredibly descriptive, description of the image 6.jpg" class="image10" style="opacity: 0.7;">
+          </a>
+        </li>
+        <li><a href="images/7.jpg" id="" class=""><img src="images/thumbs/t7.jpg" title="" alt="" style="opacity: 0.7;"></a></li><li><a href="images/8.jpg" id=""><img src="images/thumbs/t8.jpg" title="" alt="" style="opacity: 0.7;"></a></li><li><a href="images/9.jpg" id="" class=""><img src="images/thumbs/t9.jpg" title="" alt="" style="opacity: 0.7;"></a></li></ul>
+    </div>
+    <div class="ad-forward" style="opacity: 0.6;"></div></div>
+</div>
